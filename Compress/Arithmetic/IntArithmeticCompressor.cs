483474bfdf2be1690fc1c1199c2bae78d0aa1cdf
Abstrag <barmaley2.0@outlook.com>
2024-12-27 16:49:51 +0500

    Update MararCore.csproj

--- Compress/Arithmetic/IntArithmeticCompressor.cs
+++ Compress/Arithmetic/IntArithmeticCompressor.cs
@@ -1,45 +1,73 @@
-﻿
-namespace MararCore.Compress.Arithmetic
+﻿namespace MararCore.Compress.Arithmetic
 {
     public class IntArithmeticCompressor : FileProcessor
     {
-        private const byte CodeLength = 16;
-        private const byte Base = 2;
-        private const byte BitMove = 1;
-        private static readonly ulong MaxCode = (ulong)(MathF.Pow(Base, CodeLength) - 1);
-        private static readonly ulong FirstQtr = MaxCode / 4;
-        private static readonly ulong Half = MaxCode / 2;
-        private static readonly ulong ThirdQtr = MaxCode * 3 / 4;
-        private Tuple<double, double>[] FloatDictionary = new Tuple<double, double>[256];
+        private readonly byte CodeLength;
+        private readonly ulong MaxCode = 1;
+        private readonly ulong[] FrequencyDictionary = new ulong[256];
+        private readonly Tuple<ulong, ulong>[] CurrentDictionary = new Tuple<ulong, ulong>[256];
 
-        public IntArithmeticCompressor(Stream input, Stream output) : base(input, output) { }
-
-        private void InitDictionary()
+        public IntArithmeticCompressor(Stream input, Stream output, byte codeLength = 32) : base(input, output)
         {
-
-        }
-        private ulong Bml(ulong num1, int shift, ulong num2)
-        {
-            return (num1 << shift) & num2;
-        }
-        private ulong Bmlt(ulong num1, int shift, ulong num2)
-        {
-            num1 <<= shift;
-            for (int i = 1; i < shift; i++)
+            CodeLength = codeLength;
+            for (byte i = 0; i < CodeLength; i++)
             {
-                num1 |= (ulong)1 << i;
+                MaxCode *= 2;
             }
-            return num1 & num2;
         }
-        private ulong BitsPlusFollow()
+        private void WriteDictionary()
         {
-
+            foreach (ulong key in FrequencyDictionary)
+            {
+                Output.Write(BitConverter.GetBytes(key));
+            }
         }
-
+        private void InitFrequencyDictionary()
+        {
+            while (Input.Position < Input.Length)
+            {
+                FrequencyDictionary[Input.ReadByte()]++;
+            }
+        }
+        private void UpdateRangeDictionary(Tuple<ulong, ulong> baseRange)
+        {
+            double step = (baseRange.Item2 - baseRange.Item1) / (double)Input.Length;
+            CurrentDictionary[0] = new (0, (ulong)Math.Round(FrequencyDictionary[0] * step));
+            
+            for (ushort i = 1; i < 256; i++)
+            {
+                ulong length = (ulong)Math.Round(FrequencyDictionary[i] * step);
+                ulong start = CurrentDictionary[i - 1].Item2;
+                CurrentDictionary[i] = new(start, start + length);
+            }
+        }
         public override void Encode()
         {
+            BitStream bitStream = new(Output);
+            Tuple<ulong, ulong> currentRange = new(0, MaxCode);
             
+            InitFrequencyDictionary();
+            WriteDictionary();
+            UpdateRangeDictionary(currentRange);
+            Input.Position = 0;
+            
+            while (Input.Position < Input.Length)
+            {
+                if (currentRange.Item2 - currentRange.Item1 > 0)
+                {
+                    byte symbol = (byte)Input.ReadByte();
+                    currentRange = CurrentDictionary[symbol];
+                }
+                else
+                {
+                    bitStream.Write(currentRange.Item1, CodeLength);
+                    currentRange = new(0, MaxCode);
+                    UpdateRangeDictionary(currentRange);
+                }
+            }
+            
+            bitStream.Write(currentRange.Item1, CodeLength);
+            bitStream.FlushWrite();
         }
-
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,22 +1,29 @@
+using MararCore.Compress.Arithmetic;
 using MararCore.Compress.LZA;
 
 namespace MararCore
 {
     public class Program
-    {
-        private static string Origin = @"Y:\Users\bar32\Pictures\����2.png";
+    {   
+        #if false
+        private static string Origin = @"Y:\Users\bar32\Pictures\f2.png";
         private static string Encoded = @"Y:\Users\bar32\Pictures\sample.bin";
         private static string Decoded = @"Y:\Users\bar32\Pictures\figna.bin";
+        #else
+        private static string Origin = "/home/admen/Рабочий стол/MararTest/gitter.jpg";
+        private static string Encoded = "/home/admen/Рабочий стол/MararTest/f2.bin";
+        private static string Decoded = "/home/admen/Рабочий стол/MararTest/f3.bin";
+        #endif
 
         public static void Main()
         {
-            FileStream input = new(Origin, FileMode.Open);
-            FileStream output = new(Encoded, FileMode.Create);
-            FileProcessor processor = new LZ78(input, output);
-            processor.Encode();
-            Console.WriteLine($"{input.Length} -> {output.Length}");
-            input.Close();
-            output.Close();
+            FileStream f1 = new(Origin, FileMode.Open);
+            FileStream f2 = new(Encoded, FileMode.Create);
+
+            IntArithmeticCompressor compressor = new(f1, f2);
+            compressor.Encode();
+            f1.Close();
+            f2.Close();
 
             /*Console.WriteLine(Enumerable.SequenceEqual([97, 98], [97, 98]));
             List<byte[]> dictionary = new();
